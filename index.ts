8572a50928c593b8f2ed53a4a25a831bee5908c8
zakharchenkoantonina <158192333+zakharchenkoantonina@users.noreply.github.com>
2024-08-02 14:33:41 +0300

    add error messages

---
+++ index.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import router from './routes'
+
+
+const app = express()
+
+
+app.use(express.json())
+
+app.use(router)
+
+
+app.listen(3001, () => {
+    console.log('Server Running')
+})
